f8f691ca3d71a5c8d06511b1e3c1285a59277198
Steffi LaZerte <steffi@steffi.ca>
2023-03-16 17:11:52 -0500

    Update DEVTESTS

--- inst/DEVTESTS.R
+++ inst/DEVTESTS.R
@@ -1,78 +1,132 @@
+# Workflows
+#
+# m <- clean_metadata()
+# s <- clean_site_index() # From user-supplied file
+# m <- add_sites(s)
+#
+# m <- clean_metadata()
+# g <- clean_gps(m)    # From GPS summary log files
+# m <- add_sites(s)
 
-# example 1 - different date formats
+
+# eg File output ----------
+out <- readr::read_rds("../Boreal_Shield_Lowlands_transition_Selection/Meta_data_cleaned.rds")
+
+# - Only wave files
+# - GPS data added
+# - Sunset/sunrise added
+
+# eg 1 - Date sep ------------------------------------------------
 d <- "../ARUtools - Extra/ARUtools_file_examples/HudsonBayCoast_2021/"
 
 count_files(d)
 
-t <- clean_metadata(project_dir = d,
+m <- clean_metadata(project_dir = d,
                     pattern_site = "BP_ARU0(1|2)",
                     pattern_dt_sep = "(_|T)")
 
-check_meta(t)
+check_meta(m)
 
 # OR
-t <- clean_metadata(project_dir = d,
+m <- clean_metadata(project_dir = d,
                     pattern_site = "BP_ARU0(1|2)",
                     pattern_dt_sep = create_pattern_dt_sep(c("T", "_")))
 
-t <- clean_gps(t)
+g <- clean_gps(m) # Error
+g <- clean_gps(m, dist_cutoff = Inf)
+
+m <- add_sites(m, g)
+
+filter(m, is.na(longitude)) %>% select(-path) # Look at where no GPS match
+filter(g, site_id == "BP_ARU01") # Ah, no date overlap
+
+# eg 2 - 'Summary' GPS files -------------------------------------------
+d <- "../ARUtools - Extra/ARUtools_file_examples/LutherMarsh_2021/"
+count_files(d)
+
+m <- clean_metadata(project_dir = d)
+
+# Hmm, check for problems
+m$path[1:15]
+check_meta(m)
+
+# Try again
+m <- clean_metadata(project_dir = d,
+                    pattern_dt_sep = "_")
+m
+g <- clean_gps(m, dist_by = "aru_id")
 
 
-
-
-# example 2
+# eg 3 - -------------------------
 d <- "../ARUtools - Extra/ARUtools_file_examples/JamesBayLowlands_2021/"
-t <- clean_metadata(project_dir = d)
+m <- clean_metadata(project_dir = d)
 
-check_meta(t)
+check_meta(m)
 
-# example 3
+g <- clean_gps(m)
+
+# Check problematic file
+m$path[183]
+check_file(m$path[183])
+
+# Try skipping for now
+g <- clean_gps(m, skip_bad = TRUE)
+g <- clean_gps(m, skip_bad = TRUE, dist_cutoff = Inf)
+
+
+m_full <- add_sites(m, g)
+filter(m_full, n_matches > 1)
+filter(m_full, is.na(longitude))
+
+# try again
+m_full <- add_sites(m, g, dt_type = "date_time")
+filter(m_full, is.na(longitude))
+
+# eg 4 - File lists -----------------------------
 files <- readr::read_rds("../Boreal_Shield_Lowlands_transition_Selection/Transition_all_files.rds")
 
-t <- clean_metadata(
+m <- clean_metadata(
   project_files = files,
   pattern_site = "(P|Q)\\d{3}",
-  pattern_dt_sep = create_pattern_dt_sep(sep = c("T", "_")))
-t
+  pattern_dt_sep = create_pattern_dt_sep(sep = c("T", "_")),
+  subset = "ZOOM|Zoom", subset_type = "omit"
+  )
+m
 
-# example 4 - with sites from file
+# eg 5 - Site from file ----------------------
 files <- readr::read_rds("../RiverTrips_DataPrep/all_files_vec.rds")
 i <- "../RiverTrips_DataPrep/NRT2022_SMM_Logfile_Compile.xlsx"
 
-# Expect standard column names
-sites <- clean_site_index(i)
+# Some funny files 'zoom'
+t <- clean_metadata(project_files = files,
+                    pattern_dt_sep = "_")
 
-# Supply column names
-sites <- clean_site_index(i,
+filter(t, is.na(aru_id))
+
+# Omit 'zoom' files
+t <- clean_metadata(project_files = files,
+                    subset = "Zoom", subset_type = "omit",
+                    pattern_aru_id = create_pattern_aru_id(prefix = "CWS-"),
+                    pattern_dt_sep = "_")
+
+# Add sites by date
+sites <- clean_site_index(i) # Expect standard column names
+sites <- clean_site_index(i, # Supply column names
                           col_aru_id = "ARU_ID",
                           col_site_id = "SiteID_WildTrax",
-                          col_dates = c("Date_Deploy", "Date_Retrieve")) %>%
-  dplyr::mutate(aru_id = stringr::str_remove(aru_id, "^CWS-"))
+                          col_date_time = c("Date_Deploy", "Date_Retrieve"))
 
-# no sites
-t <- clean_metadata(project_files = files,
-                    pattern_dt_sep = "_")
+f <- add_sites(t, sites) # Need to omit "site_id" from by
+f <- add_sites(t, sites, by = "aru_id")
+# BerensR-3252-2 2022-06-29 - 2022-06-30
+# BerensR-3252-3 2022-06-30 - 2022-07-01
+# Where does 2022-06-30 belong? Best to use `date_time` to split
+f <- add_sites(t, sites, by = "aru_id", dt_type = "date_time")
 
-# most sites
-t <- clean_metadata(project_files = files,
-                    site_index = sites,
-                    pattern_aru_id = create_pattern_aru_id(arus = "SMM"),
-                    pattern_dt_sep = "_")
+# Still have some problems... oops, some sound files have no times, so match both...
+# Will need use input to fix these
+filter(f, n_matches > 1)
 
-# Split for diff date/time formats
-t1 <- clean_metadata(project_files = files,
-                     site_index = sites,
-                     subset = "Zoom", subset_type = "omit",
-                     pattern_aru_id = create_pattern_aru_id(arus = "SMM"),
-                     pattern_dt_sep = "_")
-
-t2 <- clean_metadata(project_files = files,
-                     subset = "Zoom",
-                     pattern_date = create_pattern_date(n_years = 2),
-                     pattern_dt_sep = "-")
-
-# t1 is great, all sites all date/times
-# t2 is problematic
-#  - no sites (no clear pattern and no aru_id to match to index)
-#  - Only some date/times
-
+# Now check non-matched - These are either a) not in the date range in sites, or b) missing a record in sites
+filter(f, is.na(site_id)) |>
+  check_meta()
