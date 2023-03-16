488009f0094fb43a2c55e12eddcfcc42622e28d3
Steffi LaZerte <steffi@steffi.ca>
2023-03-16 17:02:59 -0500

    Add gps flags, tweak messages, make checks more generic, don't use gps file (use clean_site_index)

--- R/clean_gps.R
+++ R/clean_gps.R
@@ -1,11 +1,12 @@
 #' Check and clean GPS data
 #'
-#' Check and clean GPS data either from ARU logs or user-supplied GPS data
-#' (preferred). GPS points are checked for obvious problems (expected range,
-#' distance cutoffs and timing) then attached to the meta data frame.
+#' Check and clean GPS data from ARU logs. GPS points are checked for obvious
+#' problems (expected range, distance cutoffs and timing) then attached to the
+#' meta data frame. Note that it is often safer and more reliable to create
+#' your own Site Index file including site ids, and GPS coordinates. This file
+#' can be cleaned and prepared with `clean_site_index()` instead.
 #'
 #' @param meta Data frame. Output of `clean_metadata()`.
-#' @param gps Data frame. GPS data points (optional, but recommended).
 #' @param dist_cutoff Numeric. Maximum distance (m) between GPS points within a
 #'   site. Default is 100m but can be set to `Inf` to skip.
 #' @param dist_crs Numeric. Coordinate Reference System to use when calculating
@@ -17,30 +18,50 @@
 #'
 #' @examples
 
-clean_gps <- function(meta, gps = NULL, dist_cutoff = 100, dist_crs = 3161,
+clean_gps <- function(meta = NULL,
+                      dist_cutoff = 100, dist_crs = 3161,
+                      dist_by = "site_id",
                       skip_bad = FALSE, verbose = FALSE) {
 
-  # CHECK meta
+  # TODO:
   # CHECK gps
   # CHECK dist_crs
   # Other checks...
 
-  # Clean depending on source
-  if(is_null(gps)) {
-    gps <- clean_gps_logs(meta, skip_bad, verbose)
-  } else {
-    gps <- clean_gps_df(gps, verbose)
-  }
-
-  # Check start/end dates
+  # Load, combine and clean gps files
+  gps <- clean_gps_files(meta, skip_bad, verbose)
 
   # Check distances (skips if dist_cutoff = Inf)
-  gps <- check_gps_dist(gps, crs = dist_crs, dist_cutoff = dist_cutoff)
+  gps <- check_gps_dist(gps, crs = dist_crs, dist_cutoff = dist_cutoff,
+                        dist_by = dist_by)
+
+  # Flag problems -----------------------------
+  n <- nrow(gps)
+  f_dt <- sum(is.na(gps$date_time))
+  f_coord <- sum(is.na(gps$longitude) | is.na(gps$latitude))
+  f_zero <- sum(gps$longitude == 0 | gps$latitude == 0)
+
+  if(any(c(f_dt, f_coord, f_zero) > 0)) {
+    msg <- c("Identified possible problems with GPS extraction:")
+    msg <- c(msg, report_missing(f_dt, n, "date/times"))
+    msg <- c(msg, report_missing(f_coord, n, "coordinates"))
+    if(f_zero > 0) {
+      msg <- c(
+        msg,
+        "Some coordinates detected as zero (can occur in Song Meters if not set)",
+        paste0("Replacing zero coordinates with NA (", f_zero, "/", n, ")"))
+    }
+
+    gps <- dplyr::mutate(
+      gps, dplyr::across(c("latitude", "longitude"), ~dplyr::na_if(.x, 0)))
+
+    rlang::inform(msg)
+  }
 
   gps
 }
 
-clean_gps_logs <- function(meta, skip_bad, verbose) {
+clean_gps_files <- function(meta, skip_bad, verbose) {
 
   gps <- dplyr::filter(meta, .data$type == "gps")
 
@@ -50,8 +71,11 @@ clean_gps_logs <- function(meta, skip_bad, verbose) {
       call = NULL)
   }
 
-  rlang::inform(c("No GPS data provided, using GPS log files",
-                  "*" = "Note GPS log files are notoriously unreliable..."))
+  rlang::inform(c("Note: GPS log files are notoriously unreliable... ",
+                  "Consider supplying your own GPS records and using `clean_site_index()`"))
+
+  # TODO: CHECK IF HAVE SERIAL? FIRMWARE?
+
   gps |>
     # Check columns and get skips
     check_gps_files(skip_bad) |>
@@ -68,12 +92,8 @@ clean_gps_logs <- function(meta, skip_bad, verbose) {
       gps_data = purrr::map(.data$gps_data, fmt_gps)) |>
 
     # Clean up
-    dplyr::select(-"date_time", -"date", -"path", -"skip") %>%
+    dplyr::select(-"date_time", -"date", -"skip") %>%
     tidyr::unnest("gps_data", keep_empty = TRUE)
-}
-
-clean_gps_df <- function(gps) {
-
 
 }
 
@@ -182,22 +202,33 @@ fmt_gps <- function(df) {
 #' @return Returns data frame with maximum distances between gps points at site.
 #'
 #' @noRd
-check_gps_dist <- function(gps, crs, dist_cutoff){
+check_gps_dist <- function(gps, crs, dist_cutoff, dist_by){
+
   if(dist_cutoff < Inf) {
     max_dist <- gps |>
-      dplyr::filter(!is.na(.data$longitude), !is.na(.data$latitude),
-                    !is.na(.data$site_id))
+      dplyr::filter(dplyr::if_all(
+        dplyr::any_of(c("latitude", "longitude", dist_by)), ~!is.na(.)))
 
     if(nrow(max_dist) == 0) {
+      if(!is.null(dist_by)) dist_by <- paste0(", `", dist_by, "`") else dist_by <- ""
       rlang::inform(
         c("Skipping distance check:",
-          "All records missing at least one of longitude, latitude and site_id"))
+          paste0("All records missing at least one of ",
+                 "`longitude`, `latitude`", dist_by)))
     } else {
-      max_dist <- max_dist |>
+      n <- dplyr::count(max_dist, .data$latitude, .data$longitude, .data[[dist_by]])
 
+      if(all(n$n == 1)) {
+        rlang::inform(
+          c("Skipping distance check:",
+            paste0("No records with more than one set of coordinates per `",
+                   dist_by, "`")))
+      }
+
+      max_dist <- max_dist |>
         sf::st_as_sf(coords= c("longitude", "latitude"), crs = 4326) |>
         sf::st_transform(crs) |>
-        dplyr::group_by(site_id) |>
+        dplyr::group_by(.data[[dist_by]]) |>
         dplyr::summarize(max_dist = max(sf::st_distance(geometry, geometry)),
                          .groups = 'drop') |>
         sf::st_drop_geometry()
@@ -210,7 +241,7 @@ check_gps_dist <- function(gps, crs, dist_cutoff){
             "i" = "Set `dist_cutoff` to `Inf` to skip this check (e.g. moving ARUs)"),
           call = NULL)
       }
-      gps <- dplyr::left_join(gps, max_dist, by = "site_id")
+      gps <- dplyr::left_join(gps, max_dist, by = dist_by)
     }
   }
   gps
