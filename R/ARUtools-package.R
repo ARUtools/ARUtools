d982395bf67c04fada41d464a27fa671868b38b2
Steffi LaZerte <steffi@steffi.ca>
2023-05-02 17:28:04 -0500

    Add other time patterns to default GPS

--- R/ARUtools-package.R
+++ R/ARUtools-package.R
@@ -18,7 +18,7 @@ rlang::on_load(rlang::local_use_cli())
   # Set column patterns (case insensitive)
   options(ARUtools =
             list(pat_gps_date = "date|(DD/MM/YY)",
-                 pat_gps_time = "time|(HH/MM)",
+                 pat_gps_time = "time|(HH/MM(/SS)?)|(HH:MM(:SS)?)", # Optional seconds
                  pat_gps_coords = c("lat", "lon")
             )
   )
