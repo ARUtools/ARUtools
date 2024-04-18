918f448608111335fabc1926bb85d13f7203df78
David Hope <david.hope@ec.gc.ca>
2024-04-18 13:42:36 -0400

    Add back play_random_song

---
+++ tests/testthat/test-play_random_song.R
@@ -0,0 +1,21 @@
+test_that("Playing does not affect global seed", {
+  skip_if_not(interactive())
+  expect_equal(
+    {
+      set.seed(1234)
+      runif(1)
+    },
+    {
+      set.seed(1234)
+      try(play_random_track("path/to/file/"), silent = TRUE)
+      runif(1)
+    }
+  )
+})
+
+test_that("Weird file path gives error", {
+  skip_if_not(interactive())
+  expect_error({
+    play_random_track("I feel a bad path rising")
+  })
+})
